f7956619a85c9fadbe7125fbb66117f1cf042353
Lostecho <31339626+SummerZephyr@users.noreply.github.com>
2024-09-11 10:45:56 +0800

    edit weread pake config file, add custom.js

--- src-tauri/src/inject/custom.js
+++ src-tauri/src/inject/custom.js
@@ -4,3 +4,56 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
+
+const contentWidthKey = 'cWidthz';
+
+function changeWidth() {
+    'use strict';
+
+    // 基础方法
+    function getCurrentMaxWidth(element) {
+        let currentValue = window.getComputedStyle(element).maxWidth;
+        currentValue = currentValue.substring(0, currentValue.indexOf('px'));
+        currentValue = parseInt(currentValue);
+        return currentValue;
+    }
+
+    function changeWidth() {
+        const items = document.querySelectorAll(".readerContent .app_content, .readerTopBar");
+        const targetValue = window.innerWidth * 1.0;
+        items.forEach(item => {
+            const currentValue = getCurrentMaxWidth(item);
+            if (currentValue !== targetValue) {
+                item.style['max-width'] = targetValue + 'px';
+            }
+        });
+        const myEvent = new Event('resize');
+        window.dispatchEvent(myEvent)
+    }
+
+    // 调整宽度
+    changeWidth();
+
+    // 将.readerControls元素距离右边10%
+    const readerControls = document.querySelector('.readerControls');
+    readerControls.style.right = '0%';
+    readerControls.style.left = 'unset';
+    readerControls.style.marginLeft = 'unset';
+}
+
+document.addEventListener('keydown', function(event) {
+  // 检查是否按下的是F11键
+  if (event.key === 'F11') {
+      fullScreenTrigger()
+  }
+});
+
+document.addEventListener('click', () => {
+  console.log('click docmutmen')
+  renderMenu(false)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 修改页面宽度
+    changeWidth();
+});
